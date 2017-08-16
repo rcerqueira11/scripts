d7770066d519120373675082ad6d492cf7f993f2
Ricardo Cerqueira <ricardoc@blanclabs.com>
2017-08-16 11:56:16 -0400

    subinedo el push script

---
+++ push_scripts.sh
@@ -0,0 +1,19 @@
+ #!/usr/bin/env bash
+
+echo "Comentario para el Git"
+
+read -r line || [[ -n "$line" ]]
+
+Comentario="$line"
+
+echo "Git status"
+git status
+
+echo "Git add all"
+git add --all
+
+echo "Git commit"
+git commit -m "$Comentario"
+
+echo "PUSH!"
+git push
